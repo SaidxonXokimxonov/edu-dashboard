2d28d2723727acd43eeb428af3c6a5535a222842
IsmoilQaxramonov <frontqaxramonov@gmail.com>
2025-07-10 12:52:59 +0500

    added group page

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ title, px, py, bgColor }: BtnProps) => {
   return (
     <button
       className={`bg-blue-500 text-white cursor-pointer font-semibold rounded-lg 
-        hover:bg-blue-600 ${px} ${py} ${bgColor}`}
+      ${px} ${py} ${bgColor}`}
     >
       {title}
     </button>
